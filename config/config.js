52f4d34e472582a041a18350ff226c698f951c36
babsyinks <babawarunn@yahoo.com>
2022-07-13 18:49:01 +0100

    fixed issue of changing db credentials on heroku

--- config/config.js
+++ config/config.js
@@ -1,6 +1,16 @@
 const path = require('path')
 require('dotenv').config({path:path.join('..','.env')});
-
+let DATABASE_URL
+/* let host,username,password,database */
+if(process.env.NODE_ENV === 'production'){
+  DATABASE_URL = process.env.DATABASE_URL
+/*   const dbUrlExtract = DATABASE_URL.slice(11)
+  const extract = dbUrlExtract.split(":")
+  username = extract[0]
+  password = extract[1].split("@")[0]
+  host = extract[1].split("@")[1]
+  database = extract[2].split("/")[1] */
+}
 const creds = {
   "development": {
     "username": "postgres",
@@ -18,10 +28,11 @@ const creds = {
     "dialect": "postgres"
   },
   "production": {
-    "username": process.env.DB_USERNAME,
-    "password": process.env.DB_PASSWORD,
-    "database": process.env.DB_NAME,
-    "host": process.env.DB_HOSTNAME,
+    "use_env_variable": DATABASE_URL,
+/*     "username": username,
+    "password": password,
+    "database": database,
+    "host": host, */
     "dialect": "postgres",
     dialectOptions: {
       ssl: {     
@@ -31,5 +42,4 @@ const creds = {
     }
   }
 }
-
 module.exports = creds;
