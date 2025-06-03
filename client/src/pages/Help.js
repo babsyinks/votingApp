b0c732ac2517123f81d0a9f295f65cc2f392ed72
babsyinks <babawarunn@yahoo.com>
2025-06-03 10:42:45 +0100

    ref: change background gradient color classname in Help component

--- client/src/pages/Help.js
+++ client/src/pages/Help.js
@@ -6,7 +6,7 @@ import HelpFootNote from "../features/help/components/HelpFootNote";
 
 function Help() {
   return (
-    <Block type="flex-horz" className="mnh-100vh p-2r b-l-g-blue">
+    <Block type="flex-horz" className="mnh-100vh p-2r bg-gradient-blueviolet">
       <Block className="bg-white-transparent border-rounded-16 p-2r mxw-600 w-full ta-center bs-black fadeIn">
         <HelpMessage />
         <HelpMeans />
