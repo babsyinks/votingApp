5ce6033c3229e00e22c5baebda330a482403b07b
babsyinks <babawarunn@yahoo.com>
2025-06-03 11:52:08 +0100

    feat(hook): added useTimerStates hook with its config

---
+++ client/src/features/timer/config/timerConf.js
@@ -0,0 +1,20 @@
+const timerConf = [
+  {
+    label: "Election Start Day",
+    type: "date",
+  },
+  {
+    label: "Election Start Time",
+    type: "time",
+  },
+  {
+    label: "Election End Day",
+    type: "date",
+  },
+  {
+    label: "Election End Time",
+    type: "time",
+  },
+];
+
+export default timerConf;
