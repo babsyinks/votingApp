f915e4d1e20ad217235d5ce4e982188a2a31940d
babsyinks <babawarunn@yahoo.com>
2025-06-04 14:58:01 +0100

    feat(timer/hook): created useTimerSchedule hook

---
+++ client/src/features/timer/hooks/useTimerSchedule.js
@@ -0,0 +1,38 @@
+import { useState } from "react";
+import timerConf from "../config/timerConf";
+
+function useTimerSchedule() {
+  const [startDate, setStartDate] = useState("");
+  const [startTime, setStartTime] = useState("");
+  const [endDate, setEndDate] = useState("");
+  const [endTime, setEndTime] = useState("");
+
+  const stateMap = [
+    {
+      value: startDate,
+      onChange: (e) => setStartDate(e.target.value),
+    },
+    {
+      value: startTime,
+      onChange: (e) => setStartTime(e.target.value),
+    },
+    {
+      value: endDate,
+      onChange: (e) => setEndDate(e.target.value),
+    },
+    {
+      value: endTime,
+      onChange: (e) => setEndTime(e.target.value),
+    },
+  ];
+
+  const mergedTimerState = timerConf.map((obj, i) => ({
+    ...obj,
+    value: stateMap[i].value,
+    onChange: stateMap[i].onChange,
+  }));
+
+  return { mergedTimerState, startDate, startTime, endDate, endTime };
+}
+
+export default useTimerSchedule;
