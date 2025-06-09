6a7b647e08684615d08979a31e68211d617c0b81
babsyinks <babawarunn@yahoo.com>
2025-06-09 13:24:19 +0100

    feat: add ignore actions to store configuration in rootReducer

--- client/src/app/rootReducer.js
+++ client/src/app/rootReducer.js
@@ -1,5 +1,14 @@
 import { configureStore, combineReducers } from "@reduxjs/toolkit";
-import { persistStore, persistReducer } from "redux-persist";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
 import storage from "redux-persist/lib/storage";
 import loaderReducer from "./loaderSlice";
 import userInfoReducer from "../features/user/userSlice";
@@ -25,6 +34,12 @@ const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
   reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
 
 export const persistor = persistStore(store);
