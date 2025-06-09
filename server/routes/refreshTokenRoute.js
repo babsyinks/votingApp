a1058f94cf782ee36712d49cabd4f6f7bd60a9ba
babsyinks <babawarunn@yahoo.com>
2025-06-09 12:12:41 +0100

    add route to handle auto refresh of expired tokens
    feat: add route to handle auto refresh of expired tokens

---
+++ server/routes/refreshTokenRoute.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const jwt = require("jsonwebtoken");
+
+const { setAccessTokenOnCookie } = require("../utils/setCookies");
+const { generateAccessToken } = require("../utils/tokenGenerators");
+
+const router = express.Router();
+
+router.post("/refresh", (req, res) => {
+  const refreshToken = req.cookies.refresh_token;
+  if (!refreshToken) return res.sendStatus(403);
+
+  try {
+    const user = jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET);
+    const accessToken = generateAccessToken(user);
+    setAccessTokenOnCookie({ res: res.status(200), accessToken }).json({
+      success: true,
+    });
+  } catch {
+    res.sendStatus(403);
+  }
+});
+
+module.exports = router;
