a0cd6f2491b125ea44a27cf1d5c94d5152e15893
babsyinks <babawarunn@yahoo.com>
2025-06-10 18:55:22 +0100

    feat: add VotingProcess component which now replaces ElectionDetails as the page for /vote route

---
+++ client/src/pages/VotingProcess.js
@@ -0,0 +1,24 @@
+import { useEffect } from "react";
+import {
+  timerData,
+  fetchThenSetCurrentTimerStatus,
+} from "features/timer/timerSlice";
+import { useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
+import ElectionDetails from "features/election/components/ElectionDetails";
+import PreElectionCountDown from "features/timer/components/preElectionTimer/PreElectionCountDown";
+
+function VotingProcess() {
+  const dispatch = useDispatch();
+  const timer = useSelector(timerData);
+
+  useEffect(() => {
+    dispatch(fetchThenSetCurrentTimerStatus());
+  }, [dispatch]);
+
+  if (timer.startDate)
+    return <PreElectionCountDown endTime={timer.startDate} />;
+  else return <ElectionDetails />;
+}
+
+export default VotingProcess;
