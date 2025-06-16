44741450a5d68fb20c23a5c41cdfd48b3438720c
babsyinks <babawarunn@yahoo.com>
2025-06-16 16:00:43 +0100

    feat: add toast message display after delete election button is clicked

--- client/src/features/admin/components/AdminElectionDelete.js
+++ client/src/features/admin/components/AdminElectionDelete.js
@@ -3,6 +3,7 @@ import { useSelector, useDispatch } from "react-redux";
 import { useAxios } from "hooks/useAxios";
 import { useToastMessage } from "hooks/useToastMessage";
 import { timerData, setTimerData } from "features/timer/timerSlice";
+import ToastMessage from "components/ui/ToastMessage";
 import AdminElectionDeleteWarningModal from "./AdminElectionDeleteWarningModal";
 import AdminElectionDeleteButton from "./AdminElectionDeleteButton";
 
@@ -11,7 +12,8 @@ function AdminElectionDelete() {
   const timer = useSelector(timerData);
   const dispatch = useDispatch();
   const { response, error, triggerRequest } = useAxios();
-  const { triggerSuccessToast, triggerFailureToast } = useToastMessage();
+  const { toast, triggerSuccessToast, triggerFailureToast, toastDetailsSet } =
+    useToastMessage();
 
   useEffect(() => {
     if (response) {
@@ -35,16 +37,19 @@ function AdminElectionDelete() {
     });
   };
   return (
-    timer.endDate === null && (
-      <>
-        <AdminElectionDeleteWarningModal
-          openModal={openModal}
-          setOpenModal={setOpenModal}
-          deleteElection={deleteElection}
-        />
-        <AdminElectionDeleteButton setOpenModal={setOpenModal} />
-      </>
-    )
+    <>
+      {toastDetailsSet() && <ToastMessage toast={toast} />}
+      {timer.endDate === null && (
+        <>
+          <AdminElectionDeleteWarningModal
+            openModal={openModal}
+            setOpenModal={setOpenModal}
+            deleteElection={deleteElection}
+          />
+          <AdminElectionDeleteButton setOpenModal={setOpenModal} />
+        </>
+      )}
+    </>
   );
 }
 
