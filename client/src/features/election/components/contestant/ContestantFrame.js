c389066848141e1fbf12fa4a2026f87c68f04e3b
babsyinks <babawarunn@yahoo.com>
2025-06-18 18:31:56 +0100

    feat: add className prop to ContestantFrame

--- client/src/features/election/components/contestant/ContestantFrame.js
+++ client/src/features/election/components/contestant/ContestantFrame.js
@@ -1,20 +1,18 @@
 import React from "react";
+import Block from "components/ui/Block";
 import style from "./ContestantFrame.module.css";
 /**
  * Represents a frame for a single contestant. This frame is a container having the right structure and
  * formating to hold all pertinent information about a contestant. E.g name, picture, votes, etc.
- * 
+ *
  * @param {Object} props Component prop.
+ * @param {string} [props.className] - Additional classes to use to style the frame
  * @param {React.ReactNode} props.children - Child elements to render inside the contestant frame.
  * @returns {JSX.Element} The rendered ContestantFrame component.
  */
-const ContestantFrame = ({ children }) => {
+const ContestantFrame = ({ className = "", children }) => {
   return (
-    <div
-      className={style.contestant}
-    >
-      {children}
-    </div>
+    <Block className={`${style.contestant} ${className}`}>{children}</Block>
   );
 };
 export default ContestantFrame;
