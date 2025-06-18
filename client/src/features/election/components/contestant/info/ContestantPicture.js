173bdd931e2dcdbfdfda4c9bcba7993461732771
babsyinks <babawarunn@yahoo.com>
2025-06-18 19:02:07 +0100

    style: add contestant-picture-full to ContestantPicture.module.css and implement this style in its component (ContestantPicture)

--- client/src/features/election/components/contestant/info/ContestantPicture.js
+++ client/src/features/election/components/contestant/info/ContestantPicture.js
@@ -5,14 +5,15 @@ import style from "./ContestantPicture.module.css";
  * Holds a contestant's picture formating and content.
  *
  * @param {Object} props Component props
- * @param {String} [props.picture] The link to the picture to be rendered in this component.
+ * @param {string} [props.picture] The link to the picture to be rendered in this component.
+ * @param {boolean} [props.showFullPicture] Indicates if the full picture size should be displayed
  * @returns {JSX.Element} The rendered ContestantPicture component.
  */
-const ContestantPicture = ({ picture }) => {
+const ContestantPicture = ({ picture, showFullPicture }) => {
   return (
     <div className={style["contestant-picture-wrapper"]}>
       <img
-        className={style["contestant-picture"]}
+        className={`${style["contestant-picture"]} ${showFullPicture ? style["contestant-picture-full"] : ""}`}
         src={picture}
         alt="contestant"
       />
@@ -22,5 +23,6 @@ const ContestantPicture = ({ picture }) => {
 
 ContestantPicture.propTypes = {
   picture: PropTypes.string.isRequired,
+  showFullPicture: PropTypes.bool,
 };
 export default ContestantPicture;
