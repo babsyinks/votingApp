b051b47120d51be26c362aa5178b0fbc2cb2a49c
babsyinks <babawarunn@yahoo.com>
2025-06-19 14:47:01 +0100

    feat: add ElectionStatusIndicator component to HomePage

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,16 +1,26 @@
-import React from "react";
-import { useSelector } from "react-redux";
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
 import { userAuth } from "features/auth/userAuthSlice";
+import {
+  fetchThenSetCurrentTimerStatus,
+} from "features/timer/timerSlice";
 import Main from "components/ui/Main";
 import HeroSection from "features/home/components/HeroSection";
 import FeatureSection from "features/home/components/FeatureSection";
 import HelpSection from "features/home/components/HelpSection";
+import ElectionStatusIndicator from "features/home/components/ElectionStatusIndicator";
 
 export default function HomePage() {
   const userIsAuthenticated = useSelector(userAuth);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchThenSetCurrentTimerStatus());
+  }, [dispatch]);
 
   return (
     <Main className="mnh-100vh">
+      <ElectionStatusIndicator />
       <HeroSection userIsAuthenticated={userIsAuthenticated} />
       <FeatureSection />
       <HelpSection />
