56ac8e0127c07857bc64f516ed45f0d210d8aa9b
babsyinks <babawarunn@yahoo.com>
2025-06-19 20:13:51 +0100

    feat: use useCountdownStatus hook in VotingProcess page

--- client/src/pages/VotingProcess.js
+++ client/src/pages/VotingProcess.js
@@ -1,21 +1,23 @@
 import { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import useCountdownStatus from "hooks/useCountdownStatus";
 import {
   timerData,
   fetchThenSetCurrentTimerStatus,
 } from "features/timer/timerSlice";
-import { useSelector, useDispatch } from "react-redux";
 import ElectionDetails from "features/election/components/ElectionDetails";
 import PreElectionCountDown from "features/timer/components/preElectionTimer/PreElectionCountDown";
 
 function VotingProcess() {
   const dispatch = useDispatch();
   const timer = useSelector(timerData);
+  useCountdownStatus(timer.startDate)
 
   useEffect(() => {
     dispatch(fetchThenSetCurrentTimerStatus());
   }, [dispatch]);
 
-  if (timer.startDate)
+  if (timer.startDate && timer.startDate > Date.now())
     return <PreElectionCountDown endTime={timer.startDate} />;
   else return <ElectionDetails />;
 }
