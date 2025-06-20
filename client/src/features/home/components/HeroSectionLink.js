160808f18c5d27cc2d4e5eb834258e792f9a2139
babsyinks <babawarunn@yahoo.com>
2025-06-20 17:31:26 +0100

    feat: dynamically render button names and links based on election status

--- client/src/features/home/components/HeroSectionLink.js
+++ client/src/features/home/components/HeroSectionLink.js
@@ -1,17 +1,26 @@
+import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
+import { electionStatus } from "features/election/electionSlice";
 import Block from "../../../components/ui/Block";
 import Button from "../../../components/ui/Button";
 
 export default function HeroSectionLink({ userIsAuthenticated }) {
+  const statusOfElection = useSelector(electionStatus);
+  const btnStateMsg = { primary: "Voting", secondary: "Vote" };
+  const electionEnded = statusOfElection === "active_election_ended";
+  if (electionEnded) {
+    btnStateMsg.primary = "Results";
+    btnStateMsg.secondary = "View Results";
+  }
   return (
     <Block className="mt-3r">
       {userIsAuthenticated ? (
-        <Link to="/vote">
-          <Button className="primary-btn">Go to Voting Page</Button>
+        <Link to={electionEnded ? "/results" : "/vote"}>
+          <Button className="primary-btn">{`Go to ${btnStateMsg.primary} Page`}</Button>
         </Link>
       ) : (
         <Link to="/login">
-          <Button className="secondary-btn">Sign In to Vote</Button>
+          <Button className="secondary-btn">{`Sign In to ${btnStateMsg.secondary}`}</Button>
         </Link>
       )}
     </Block>
