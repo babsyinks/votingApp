95c7705884172f6acb716ba0ffd5806eeba41848
babsyinks <babawarunn@yahoo.com>
2025-06-20 17:12:36 +0100

    feat: convert ResultsNone component to a generic component for components that want to render a state showing results of the election can not be rendered at the moment

--- client/src/features/results/components/ResultsNone.js
+++ client/src/features/results/components/ResultsNone.js
@@ -5,19 +5,14 @@ import Paragraph from "components/ui/Paragraph";
 import Button from "components/ui/Button";
 import Container from "layout/Container";
 
-export default function ResultsNone() {
+export default function ResultsNone({ heading, content }) {
   const navigate = useNavigate();
   return (
     <Container height="auto">
       <Heading type="h1" className="text-red-cool ta-center">
-        There Is No Election Result
+        {heading}
       </Heading>
-      <Paragraph className="p-10 w-50p">
-        You are seeing this page because no live election has been held or the
-        last live election results have been deleted. To start a new live
-        election, contact the admin through the help page and request for a new
-        live election to be scheduled.
-      </Paragraph>
+      <Paragraph className="p-10 w-50p">{content}</Paragraph>
       <Heading type="h2" className="p-10">
         What Would You Like To Do?
       </Heading>
