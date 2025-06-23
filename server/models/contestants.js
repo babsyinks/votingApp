b7569a267a97eadeec68320e8b1e71501a667067
babsyinks <babawarunn@yahoo.com>
2025-06-23 19:35:01 +0100

    ref(docstring): remove unnecessary generic docstring across the sequalize models

--- server/models/contestants.js
+++ server/models/contestants.js
@@ -2,11 +2,6 @@
 const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class Contestants extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate(models) {
       Contestants.hasOne(models.Votes, {
         foreignKey: "contestant_id",
--- server/models/votes.js
+++ server/models/votes.js
@@ -2,11 +2,6 @@
 const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class Votes extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate(models) {
       Votes.belongsTo(models.User, {
         foreignKey: "user_id",
