2420880544681ea32b680057485978313eb9bc4b
babsyinks <babawarunn@yahoo.com>
2025-06-27 16:29:19 +0100

    feat: add function to send sign up code to user mail

---
+++ server/helpers/sendSignupCode.js
@@ -0,0 +1,12 @@
+import transporter from "./mailTransporter";
+
+async function sendSignupCode({ to, code }) {
+  await transporter.sendMail({
+    from: "'Demo Voting' <no-reply@demo-voting.com>",
+    to,
+    subject: "Your verification code",
+    html: `<p>Your code is <strong>${code}</strong>. It expires in 10 minutes.</p>`,
+  });
+}
+
+module.exports = sendSignupCode;
