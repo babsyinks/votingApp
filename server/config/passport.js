48fa3d64e5b48ab5242fae389fc576e09f58a58b
babsyinks <babawarunn@yahoo.com>
2025-06-27 18:22:55 +0100

    ref(path): set proper import path for passport strategies

--- server/config/passport.js
+++ server/config/passport.js
@@ -1,8 +1,8 @@
 const passport = require("passport");
 
-const getFacebookStrategy = require("./strategies/social/FacebookStrategy");
-const githubStrategy = require("./strategies/social/GitHubStrategy");
-const getGoogleStrategy = require("./strategies/social/GoogleStrategy");
+const getFacebookStrategy = require("../strategies/social/FacebookStrategy");
+const githubStrategy = require("../strategies/social/GitHubStrategy");
+const getGoogleStrategy = require("../strategies/social/GoogleStrategy");
 
 passport.use(getFacebookStrategy());
 passport.use(githubStrategy());
