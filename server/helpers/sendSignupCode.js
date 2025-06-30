ab75bbfb62172ca65685dbd427e9bb9c5f6c0df0
babsyinks <babawarunn@yahoo.com>
2025-06-30 11:17:00 +0100

    feat: update sendSignupCode.js to use brevo for sending OTP instead of using SendGrid

--- server/helpers/sendSignupCode.js
+++ server/helpers/sendSignupCode.js
@@ -1,12 +1,23 @@
-const transporter = require("./mailTransporter");
+const apiInstance = require("../config/brevo");
+const logger = require("../utils/logger");
 
-async function sendSignupCode({ to, code }) {
-  await transporter.sendMail({
-    from: "'Demo Voting' <no-reply@demo-voting.com>",
-    to,
-    subject: "Your verification code",
-    html: `<p>Your code is <strong>${code}</strong>. It expires in 10 minutes.</p>`,
-  });
+async function sendSignupCode({ toEmail, otpCode }) {
+  const sendSmtpEmail = {
+    to: [{ email: toEmail }],
+    sender: {
+      name: "VoteNow Voting App",
+      email: process.env.BREVO_VERIFIED_SENDER_EMAIL,
+    },
+    subject: "Your One-Time Password (OTP)",
+    htmlContent: `<p>Your OTP is <strong>${otpCode}</strong>. It will expire in 10 minutes.</p>`,
+  };
+
+  try {
+    const response = await apiInstance.sendTransacEmail(sendSmtpEmail);
+    logger.info(`Email sent to ${toEmail}`, response);
+  } catch (error) {
+    logger.error(`Error sending email to ${toEmail}`, error);
+  }
 }
 
 module.exports = sendSignupCode;
