e00c4e3bcd7f847c8054aa3763997dfc20ad5391
babsyinks <babawarunn@yahoo.com>
2025-07-03 19:44:47 +0100

    feat: add AlternativeAccessMeans component which is a reusable component for providing an alternative access suggestion

---
+++ client/src/features/auth/components/AlternativeAccessMeans.js
@@ -0,0 +1,13 @@
+import { useNavigate } from "react-router-dom";
+import Button from "components/ui/Button";
+import Paragraph from "components/ui/Paragraph";
+
+export default function AlternativeAccessMeans({ question, btnLabel, route }) {
+  const navigate = useNavigate();
+
+  return (
+    <Paragraph className="text-sm">
+      {question} <Button onClick={() => navigate(route)}>{btnLabel}</Button>
+    </Paragraph>
+  );
+}
