cf99d057d2aaf59e94ecc94c97d7f6c97f94124b
babsyinks <babawarunn@yahoo.com>
2025-07-03 18:04:28 +0100

    doc: add docstring for props.type in the Span ui component

--- client/src/components/ui/Span.js
+++ client/src/components/ui/Span.js
@@ -1,10 +1,13 @@
 import React from "react";
 import PropTypes from "prop-types";
+import defaultStyle from "./Base.module.css";
+import getCompClasses from "../../util/getCompClasses";
 
 /**
  * A span element for inline text or components with optional styling.
  *
  * @param {Object} props - Component props.
+ * @param {String} [props.type] - The type of the span. E.g inline-block.
  * @param {string} [props.className] - ClassName(s) for styling.
  * @param {Object} [props.style] - Inline styles.
  * @param {function} [props.onClick] - The function that runs whenever a span is clicked.
