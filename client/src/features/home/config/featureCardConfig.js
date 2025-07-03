cd857e2b51542e743918c728e221100f0285ccff
babsyinks <babawarunn@yahoo.com>
2025-07-03 20:01:27 +0100

    feat: add feature card config file

--- client/src/features/home/config/featureCardConfig.js
+++ client/src/features/home/config/featureCardConfig.js
@@ -1,23 +1,31 @@
+import { v4 as uuidv4 } from "uuid";
+
+const mapContents = (arr) => arr.map((content) => ({ id: uuidv4(), content }));
+
+const contents1 = [
+  "Transparent voting process",
+  "Real-time vote counting",
+  "Secure authentication for all voters",
+  "Intuitive user-friendly voting interface",
+  "Flexibility in setting election period",
+];
+
+const contents2 = [
+  "Real-time voting statistics",
+  "Candidate manifestos and profiles",
+  "Prevention of double voting",
+  "Easy-to-access results dashboard",
+  "Election start and end countdown timers",
+];
+
 const items = {
   item1: {
     title: "What You Can Expect:",
-    contents: [
-      "Transparent voting process",
-      "Real-time vote counting",
-      "Secure authentication for all voters",
-      "Intuitive user-friendly voting interface",
-      "Flexibility in setting election period",
-    ],
+    contents: mapContents(contents1),
   },
   item2: {
     title: "Features of VoteNow:",
-    contents: [
-      "Real-time voting statistics",
-      "Candidate manifestos and profiles",
-      "Prevention of double voting",
-      "Easy-to-access results dashboard",
-      "Election start and end countdown timers",
-    ],
+    contents: mapContents(contents2),
   },
 };
 
