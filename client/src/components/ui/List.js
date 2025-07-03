085c4d49146a0fc633a5f6aa073e84cf2d4b347b
babsyinks <babawarunn@yahoo.com>
2025-07-03 17:59:44 +0100

    feat: use Li component in List and give priority to using uuid id over indexes for value of 'key' attribute

--- client/src/components/ui/List.js
+++ client/src/components/ui/List.js
@@ -1,5 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
+import Li from "./Li";
 import defaultStyle from "./List.module.css";
 import getCompClasses from "../../util/getCompClasses";
 
@@ -10,17 +11,29 @@ import getCompClasses from "../../util/getCompClasses";
  * @param {"ul"|"ol"} [props.as] - Determines whether it's a <ul> or <ol>.
  * @param {String} [props.className] - Additional class names.
  * @param {Object} [props.style] - Inline styles.
- * @param {React.ReactNode} props.children - List items (should be <li> elements).
+ * @param {Array} [props.items] - List items.
+ * @param {React.ReactNode} [props.children] - Optional children (should be <li> elements). One of items
+ * or children must be set.
  * @returns {JSX.Element}
  */
-export default function List({ as = "ul", className = "", style = {}, children }) {
+export default function List({
+  as = "ul",
+  className = "",
+  style = {},
+  items = [],
+  children,
+}) {
   const Tag = as;
   return (
     <Tag
       className={`${defaultStyle[as]} ${getCompClasses(defaultStyle, className)}`}
       style={style}
     >
-      {children}
+      {children
+        ? children
+        : items.map((item, i) => (
+            <Li key={item?.id || i}>{item?.content || item}</Li>
+          ))}
     </Tag>
   );
 }
@@ -29,5 +42,6 @@ List.propTypes = {
   as: PropTypes.oneOf(["ul", "ol"]),
   className: PropTypes.string,
   style: PropTypes.object,
-  children: PropTypes.node.isRequired,
+  items: PropTypes.array,
+  children: PropTypes.node,
 };
