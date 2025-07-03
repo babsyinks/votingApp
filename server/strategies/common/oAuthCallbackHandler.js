8d5e77b77d7bf2fe78b31498d683bffe01692113
babsyinks <babawarunn@yahoo.com>
2025-07-03 19:30:49 +0100

    feat: add the common handler for oAuth strategies

---
+++ server/strategies/common/oAuthCallbackHandler.js
@@ -0,0 +1,34 @@
+const { generateTokensAndRedirect } = require("../../helpers/authRouteHelpers");
+
+const FAILURE_REDIRECT = "http://localhost:3000/login";
+const SUCCESS_REDIRECT = "http://localhost:3000/oauth-success";
+
+const sessionOff = { session: false };
+
+const handleOauthCallback = ({
+  successRedirectUri = SUCCESS_REDIRECT,
+  failureRedirectUri = FAILURE_REDIRECT,
+} = {}) => {
+  return (req, res, next) => (err, user, info) => {
+    if (err || !user) {
+      let errorMessage = err?.message || "Authentication failed";
+      if (err?.errors) {
+        errorMessage = err.errors.map((e) => e.message).join("; ");
+      }
+      return res.redirect(
+        `${failureRedirectUri}?error=${encodeURIComponent(errorMessage)}`,
+      );
+    }
+
+    return generateTokensAndRedirect({
+      res,
+      user,
+      redirectUri: successRedirectUri,
+    });
+  };
+};
+
+module.exports = {
+  sessionOff,
+  handleOauthCallback,
+};
