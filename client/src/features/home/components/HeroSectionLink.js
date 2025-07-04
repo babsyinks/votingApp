0514924ecabf399acdf11403831cd5f874232ce1
babsyinks <babawarunn@yahoo.com>
2025-07-04 20:00:36 +0100

    feat: change appearance of buttons to display on homepage and when they should display

--- client/src/features/home/components/HeroSectionLink.js
+++ client/src/features/home/components/HeroSectionLink.js
@@ -1,3 +1,4 @@
+import { useState, useEffect } from "react";
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import { electionStatus } from "features/election/electionSlice";
@@ -6,22 +7,40 @@ import Button from "../../../components/ui/Button";
 
 export default function HeroSectionLink({ userIsAuthenticated }) {
   const statusOfElection = useSelector(electionStatus);
-  const btnStateMsg = { primary: "Voting", secondary: "Vote" };
   const electionEnded = statusOfElection === "active_election_ended";
-  if (electionEnded) {
-    btnStateMsg.primary = "Results";
-    btnStateMsg.secondary = "View Results";
-  }
+  const [btnState, setBtnState] = useState({
+    msg: "Voting",
+    link: "/vote",
+    class: "secondary-btn",
+  });
+
+  useEffect(() => {
+    if (electionEnded) {
+      setBtnState({
+        msg: "Results",
+        link: "/results",
+        class: "neutral-btn bg-grey-mute",
+      });
+    }
+  }, [electionEnded]);
+
   return (
     <Block className="mt-3r">
       {userIsAuthenticated ? (
-        <Link to={electionEnded ? "/results" : "/vote"}>
-          <Button className="primary-btn">{`Go to ${btnStateMsg.primary} Page`}</Button>
+        <Link to={btnState.link}>
+          <Button
+            className={btnState.class}
+          >{`Go to ${btnState.msg} Page`}</Button>
         </Link>
       ) : (
-        <Link to="/login">
-          <Button className="secondary-btn">{`Sign In to ${btnStateMsg.secondary}`}</Button>
-        </Link>
+        <Block type="flex-horz-fs" className="gap-1r">
+          <Link to="/login">
+            <Button className="neutral-btn bg-blueviolet-cool">{`Sign In`}</Button>
+          </Link>
+          <Link to="/signup-start">
+            <Button className="primary-btn">{`Sign up`}</Button>
+          </Link>
+        </Block>
       )}
     </Block>
   );
