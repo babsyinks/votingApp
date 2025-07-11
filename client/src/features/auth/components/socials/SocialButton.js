6135620545ea63d60f035bfdafea0116e9b597b7
babsyinks <babawarunn@yahoo.com>
2025-07-11 16:43:34 +0100

    feat: add socials components

---
+++ client/src/features/auth/components/socials/SocialButton.js
@@ -0,0 +1,33 @@
+import AuthButton from "../AuthButton";
+import I from "components/ui/I";
+import Span from "components/ui/Span";
+import useWindowSize from "hooks/useWindowSize";
+
+const icons = {
+  Google: <I className="fab fa-google"></I>,
+  Facebook: <I className="fab fa-facebook-f"></I>,
+  Github: <I className="fab fa-github"></I>,
+};
+
+export default function SocialButton({ type }) {
+  const socialLogin = () => {
+    window.location.href = `http://localhost:3001/auth/${type.toLowerCase()}`;
+  };
+  const { width } = useWindowSize();
+  let contentWidth = "w-60p";
+  if (width <= 400) {
+    contentWidth = "w-70p";
+  }
+
+  return (
+    <AuthButton
+      onClick={socialLogin}
+      className={`btn-social ${type.toLowerCase()}`}
+    >
+      <Span type="flex-horz" className="w-full">
+        <Span className="w-10p mr-5">{icons[type]}</Span>
+        <Span className={contentWidth}>{`Continue with ${type}`}</Span>
+      </Span>
+    </AuthButton>
+  );
+}
