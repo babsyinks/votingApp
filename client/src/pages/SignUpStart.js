96dbc5ec9f7c39a92e3d4f5a20b1e5a34e977ff3
babsyinks <babawarunn@yahoo.com>
2025-07-11 18:19:08 +0100

    ref: use AuthFrame reusable component to wrap child components and use AuthHeading instead of the generic Heading component

--- client/src/pages/SignUpStart.js
+++ client/src/pages/SignUpStart.js
@@ -1,16 +1,14 @@
-import Heading from "components/ui/Heading";
-import Block from "components/ui/Block";
-import SignUpStartAccountExists from "features/auth/components/SignUpStartAccountExists";
-import SignUpStartAccountDoesNotExist from "features/auth/components/SignUpStartAccountDoesNotExist";
+import AuthFrame from "features/auth/components/AuthFrame";
+import AuthHeading from "features/auth/components/AuthHeading";
+import SignUpStartAccountExists from "features/auth/components/signup/SignUpStartAccountExists";
+import SignUpStartAccountDoesNotExist from "features/auth/components/signup/SignUpStartAccountDoesNotExist";
 
 export default function SignUpStart() {
   return (
-    <Block className="space-y-4 max-w-md mx-auto">
-      <Heading type="h1" className="text-2xl font-semibold">
-        Create your account
-      </Heading>
+    <AuthFrame>
+      <AuthHeading>Create your account</AuthHeading>
       <SignUpStartAccountDoesNotExist />
       <SignUpStartAccountExists />
-    </Block>
+    </AuthFrame>
   );
 }
