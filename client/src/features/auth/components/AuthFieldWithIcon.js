213109e3ee5ae7b56cf023b0f81a1436b9d6e44a
babsyinks <babawarunn@yahoo.com>
2025-07-14 18:02:35 +0100

    feat: update AuthFieldWithIcon component with rightIcon prop

--- client/src/features/auth/components/AuthFieldWithIcon.js
+++ client/src/features/auth/components/AuthFieldWithIcon.js
@@ -1,9 +1,10 @@
 import InputWithIcon from "components/ui/InputWithIcon";
 
-export default function AuthFieldWithIcon({ type = "text", ...otherProps }) {
+export default function AuthFieldWithIcon({ type = "text", rightIcon, ...otherProps }) {
   return (
     <InputWithIcon
       type={type}
+      rightIcon={rightIcon}
       {...otherProps}
       className="bg-blue-faded neumorphic-input mb-2r"
     />
