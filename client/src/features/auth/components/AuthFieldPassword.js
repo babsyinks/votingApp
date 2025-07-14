9cbb78a019281318982b73dcf9e58c6b88f990c1
babsyinks <babawarunn@yahoo.com>
2025-07-14 17:59:13 +0100

    feat: add toggling of password input between input type password and input type  text

--- client/src/features/auth/components/AuthFieldPassword.js
+++ client/src/features/auth/components/AuthFieldPassword.js
@@ -1,13 +1,30 @@
+import { useState } from "react";
 import AuthFieldWithIcon from "./AuthFieldWithIcon";
+import I from "components/ui/I";
 
 export default function AuthFieldPassword({ value, onChange }) {
+  const [visible, setVisible] = useState(false);
+
+  const toggleVisibility = () => {
+    setVisible((prev) => !prev);
+  };
+
   return (
     <AuthFieldWithIcon
-      type="password"
+      type={visible ? "text" : "password"}
       value={value}
       onChange={onChange}
       placeholder="Password"
       iconClass="fa-lock"
+      rightIcon={
+        <I
+          className={`fas ${visible ? "fa-eye-slash" : "fa-eye"}`}
+          onClick={toggleVisibility}
+          withAccessibility
+          role="button"
+          ariaLabel="Toggle password visibility"
+        />
+      }
     />
   );
 }
