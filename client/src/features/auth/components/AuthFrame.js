424f0c8e0be899f526a4736a7458eabe96f90bee
babsyinks <babawarunn@yahoo.com>
2025-07-14 18:04:43 +0100

    feat: update AuthFrame with type and isFull prop

--- client/src/features/auth/components/AuthFrame.js
+++ client/src/features/auth/components/AuthFrame.js
@@ -1,10 +1,16 @@
 import Block from "components/ui/Block";
 
-export default function AuthFrame({ className = "", children }) {
+export default function AuthFrame({
+  className = "",
+  type = "flex-horz",
+  isFull = true,
+  children,
+}) {
+  const minHeight = isFull ? "mnh-100vh" : "";
   return (
     <Block
-      type="flex-horz"
-      className={`bg-blue-faded mnh-100vh p-1xr flex ${className}`}
+      type={type}
+      className={`bg-blue-faded ${minHeight} p-1xr flex ${className}`}
     >
       <Block className="bg-blue-faded border-rounded-1r bs-blue-faded w-full mxw-28 p-2r mt-1p5r">
         {children}
