35d7b8d9e3dfad7f28ed714923efa76bf3f7c547
babsyinks <babawarunn@yahoo.com>
2025-07-16 18:10:40 +0100

    ref: use newly created sendEmail functionality to replace the direct implementation of email sending in sendSignupCode.js

--- server/helpers/sendSignupCode.js
+++ server/helpers/sendSignupCode.js
@@ -1,23 +1,9 @@
-const apiInstance = require("../config/brevo");
-const logger = require("../utils/logger");
+const sendEmail = require("./sendEmail");
 
 async function sendSignupCode({ toEmail, otpCode }) {
-  const sendSmtpEmail = {
-    to: [{ email: toEmail }],
-    sender: {
-      name: "VoteNow Voting App",
-      email: process.env.BREVO_VERIFIED_SENDER_EMAIL,
-    },
-    subject: "Your One-Time Password (OTP)",
-    htmlContent: `<p>Your OTP is <strong>${otpCode}</strong>. It will expire in 10 minutes.</p>`,
-  };
-
-  try {
-    const response = await apiInstance.sendTransacEmail(sendSmtpEmail);
-    logger.info(`Email sent to ${toEmail}`, response);
-  } catch (error) {
-    logger.error(`Error sending email to ${toEmail}`, error);
-  }
+  const subject = "Your One-Time Password (OTP)";
+  const htmlContent = `<p>Your OTP is <strong>${otpCode}</strong>. It will expire in 10 minutes.</p>`;
+  await sendEmail({ toEmail, subject, htmlContent });
 }
 
 module.exports = sendSignupCode;
