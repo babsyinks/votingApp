b5b3c213c3cf9ef8e45c2289055c55c4a048e4f3
babsyinks <babawarunn@yahoo.com>
2025-07-16 19:02:26 +0100

    feat: add all newly created pages to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -3,11 +3,18 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import HomePage from "./pages/HomePage";
 import Admin from "./pages/Admin";
-import ElectionTimeSetter from "./ElectionTimeSetter";
 import Help from "./pages/Help";
-import NotFound from "./NotFound";
-import RegisterOrLogin from "./RegisterOrLogin";
-import ElectionDetails from "./pages/ElectionDetails";
+import NotFound from "./pages/NotFound";
+import ElectionTimerSettings from "./pages/ElectionTimerSettings";
+import VotingProcess from "pages/VotingProcess";
+import Results from "pages/Results";
+import Register from "pages/Register";
+import SignIn from "pages/SignIn";
+import SignUpStart from "pages/SignUpStart";
+import VerifyCode from "pages/VerifyCode";
+import OAuthSuccess from "pages/OAuthSuccess";
+import ForgotPassword from "pages/ForgotPassword";
+import ResetPassword from "pages/ResetPassword";
 
 function App() {
   return (
@@ -15,11 +22,22 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route exact path="/" element={<HomePage />} />
-          <Route exact path="/login" element={<RegisterOrLogin />} />
+          <Route exact path="/register" element={<Register />} />
+          <Route exact path="/signin" element={<SignIn />} />
+          <Route exact path="/signup-start" element={<SignUpStart />} />
+          <Route exact path="/verify-code" element={<VerifyCode />} />
+          <Route exact path="/oauth-success" element={<OAuthSuccess />} />
+          <Route exact path="/forgot-password" element={<ForgotPassword />} />
+          <Route
+            exact
+            path="/reset-password/:resetCode"
+            element={<ResetPassword />}
+          />
           <Route exact path="/admin" element={<Admin />} />
-          <Route exact path="/vote" element={<ElectionDetails />} />
-          <Route exact path="/time" element={<ElectionTimeSetter />} />
+          <Route exact path="/vote" element={<VotingProcess />} />
+          <Route exact path="/time" element={<ElectionTimerSettings />} />
           <Route exact path="/help" element={<Help />} />
+          <Route exact path="/results" element={<Results />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
