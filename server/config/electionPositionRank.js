6f9cbf98fba0b4219f59f825ff78baca1672975f
babsyinks <babawarunn@yahoo.com>
2025-07-21 13:19:24 +0100

    feat: add configuration for position ranks

---
+++ server/config/electionPositionRank.js
@@ -0,0 +1,13 @@
+module.exports = {
+  president: 0,
+  "vice president": 1,
+  "general secretary": 2,
+  "assistant general secretary": 3,
+  "national treasurer": 4,
+  "national financial secretary": 5,
+  "national social welfare officer": 6,
+  "national public relations officer": 7,
+  "national legal adviser": 8,
+  "national internal auditor": 9,
+  "chief whip": 10,
+};
