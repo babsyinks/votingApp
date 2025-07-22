7e184e9434d83f3c101996ad0cefc3531ba001e6
babsyinks <babawarunn@yahoo.com>
2025-07-22 17:54:19 +0100

    feat: create timerController, update timerService with new methods to manipulate timer and update timerRoute with new timerController

--- server/routes/timerRoutes.js
+++ server/routes/timerRoutes.js
@@ -1,45 +1,16 @@
 const express = require("express");
 
+const timerController = require("../controllers/timerController");
 const { checkAuthorizationStatus } = require("../middleware/auth");
-const { Timer } = require("../models");
+
 const router = express.Router();
 
 router.use(express.json());
 
-router.post("/set", checkAuthorizationStatus, async (req, res) => {
-  const { startDate, endDate } = req.body;
-  if (!startDate || !endDate) {
-    throw new Error("Start and end dates must be set!");
-  }
-  const timer = await Timer.findAll();
-  if (timer.length === 0) {
-    await Timer.create(req.body);
-  } else {
-    const timerObj = timer[0];
-    timerObj.set(req.body);
-    await timerObj.save();
-  }
-  res.json({ startDate, endDate });
-});
+router.post("/set", checkAuthorizationStatus, timerController.setTimer);
 
-router.get("/cancel", checkAuthorizationStatus, async (req, res) => {
-  await Timer.destroy({ truncate: true });
-  res.json({});
-});
+router.get("/status", timerController.getTimerStatus);
 
-router.get("/status", async (req, res) => {
-  const electionTimer = await Timer.findAll();
-  let electionObj;
-  const timer = electionTimer[0];
-  if (electionTimer.length === 0) {
-    electionObj = {};
-  } else {
-    electionObj = {
-      startDate: timer.startDate ? new Date(timer.startDate).getTime() : timer.startDate,
-      endDate: timer.endDate ? new Date(timer.endDate).getTime() : timer.endDate,
-    };
-  }
-  res.json(electionObj);
-});
+router.delete("/cancel", checkAuthorizationStatus, timerController.cancelTimer);
 
 module.exports = router;
--- server/services/timerService.js
+++ server/services/timerService.js
@@ -13,8 +13,40 @@ module.exports = (Timer) => {
      * @returns {Promise<Array>}
      */
     async getAllTimers() {
-      const timers = Timer.findAll();
+      const timers = await Timer.findAll();
       return timers.map((timer) => timer.toJSON());
+    },
+
+    /**
+     * Find timer by ID.
+     *
+     * @param {number} id id of the Timer to find
+     * @returns {Promise<Timer>}
+     */
+    async findTimerById(id) {
+      const timer = await Timer.findOne({ where: { id } });
+      return timer.toJSON();
+    },
+
+    /**
+     * Create a new timer
+     *
+     * @param {Timer} timer timer configuration object
+     * @returns {Promise<Timer>}
+     */
+    async createTimer(timer) {
+      return Timer.create(timer);
+    },
+
+    /**
+     * Update existing timer
+     *
+     * @param {Timer} timer The existing timer
+     * @param {Object} update The object containing the update information
+     */
+    async updateExistingTimer(timer, update) {
+      timer.set(update);
+      await timer.save();
     },
 
     /**
