b609b1f173e7215365912f3182bf3a278c8e72f8
babsyinks <babawarunn@yahoo.com>
2025-07-28 12:40:22 +0100

    ref: remove unneeded footer component

--- client/src/layout/Footer.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Footer({ textColor = "white" }) {
-  return (
-    <footer style={{ color: textColor }}>
-      &copy; Corestack Tech {new Date().getFullYear()}
-    </footer>
-  );
-}
