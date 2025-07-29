1be624c6a1aa8b808c25c8a74b9bab34abaf3b95
babsyinks <babawarunn@yahoo.com>
2025-07-29 18:00:26 +0100

    ref: rename component to ResetPassword

--- client/src/pages/ResetPassword.js
+++ client/src/pages/ResetPassword.js
@@ -3,7 +3,7 @@ import AuthFrame from "features/auth/components/AuthFrame";
 import AuthHeading from "features/auth/components/AuthHeading";
 import ForgotPasswordResetPassword from "features/auth/components/forgotPassword/ForgotPasswordResetPassword";
 
-export default function ForgotPassword() {
+export default function ResetPassword() {
   const [bottomSpacingClass, setBottomSpacingClass] = useState("");
   return (
     <AuthFrame className={bottomSpacingClass}>
