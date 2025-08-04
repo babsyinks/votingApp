52ef240c961bf3f13ba6d979f640d30277d5076f
babsyinks <babawarunn@yahoo.com>
2025-08-04 16:56:57 +0100

    feat: replace span with the Span ui component

--- client/src/features/election/components/contestant/info/ContestantBasicInfo.js
+++ client/src/features/election/components/contestant/info/ContestantBasicInfo.js
@@ -1,18 +1,21 @@
 import React from "react";
 import PropTypes from "prop-types";
+import Span from "components/ui/Span";
 import ContestantTextInfoWrapper from "./ContestantTextInfoWrapper";
 import style from "./ContestantBasicInfo.module.css";
 /**
- * Holds a contestant's basic information, and formats it. E.g the name, age, etc.
+ * Holds a contestant's basic information, and formats it. E.g the name.
  *
  * @param {Object} props Component props
- * @param {String} [props.type] The basic info type, e.g "Name".
+ * @param {string} [props.type] The basic info type, e.g "Name".
+ * @param {string} [props.value] The value of the type provided. E.g if type is name, the value could
+ * be 'John'.
  * @returns {JSX.Element} The rendered ContestantBasicInfo component.
  */
 const ContestantBasicInfo = ({ type, value }) => {
   return (
     <ContestantTextInfoWrapper>
-      {type}: <span className={style["basic-info"]}>{value}</span>
+      {type}: <Span className={style["basic-info"]}>{value}</Span>
     </ContestantTextInfoWrapper>
   );
 };
