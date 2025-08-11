1e6165ffed85e11ae1e1d8031933c286d7c1f486
babsyinks <babawarunn@yahoo.com>
2025-08-11 12:06:40 +0100

    feat: set only absolute values on days duration returned from the getTimeStatus helper

--- client/src/features/timer/helpers/getTimeStatus.js
+++ client/src/features/timer/helpers/getTimeStatus.js
@@ -4,8 +4,8 @@ const getTimeStatus = (endTime) => {
   const startTime = Date.now() / 1000; // use UNIX timestamp in seconds
   const remainingTime = endTime / 1000 - startTime;
   const days = Math.ceil(remainingTime / daySeconds);
-  const daysDuration = days * daySeconds;
+  const daysDuration = Math.abs(days * daySeconds);
   return { remainingTime, daysDuration };
 };
 
-export default getTimeStatus
+export default getTimeStatus;
