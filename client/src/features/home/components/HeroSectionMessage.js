9e8cf71b6f539832b4912c9810661516cec6a828
babsyinks <babawarunn@yahoo.com>
2025-08-16 14:33:27 +0100

    style: add new styles to visually improve hero message

--- client/src/features/home/components/HeroSectionMessage.js
+++ client/src/features/home/components/HeroSectionMessage.js
@@ -1,13 +1,14 @@
 import Heading from "components/ui/Heading";
 import Paragraph from "components/ui/Paragraph";
+import Span from "components/ui/Span";
 
 export default function HeroSectionMessage() {
   return (
     <>
       <Heading type="h1" className="text-4xl fw-700 mb-1p5r ta-left">
-        Welcome to VoteNow
+        Welcome to <Span className="text-aqua">VoteNow</Span>
       </Heading>
-      <Paragraph className="text-xl mb-2p5r text-grey-dark"> 
+      <Paragraph className="text-xl mb-2p5r paragraph-white paragraph-md"> 
         Your secure and reliable online voting platform. Whether you're running
         a student election, hosting a community poll, or conducting an
         organizational vote, VoteNow is built to handle it with ease and
